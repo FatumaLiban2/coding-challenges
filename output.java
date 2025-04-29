b1166be1e2b4dcf3a9457775088bd7bb982b5b42
FatumaLiban2 <fatuma.omar@strathmore.edu>
2025-04-29 09:11:28 +0300

    Add files via upload

---
+++ output.java
@@ -0,0 +1,12 @@
+
+//puts the message on the screen
+public class output{
+    void greetUser(){
+        System.out.println("Welcome Fatuma");
+    }
+      //main method
+      public static void main(String[] args){
+    (new output()).greetUser();
+}
+
+}
