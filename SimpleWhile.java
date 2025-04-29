9e91fe043010ef65c47bcddc524b2a6bbeb9e352
FatumaLiban2 <fatuma.omar@strathmore.edu>
2025-04-29 09:16:19 +0300

    Add files via upload

---
+++ SimpleWhile.java
@@ -0,0 +1,11 @@
+    public class SimpleWhile {
+        public static void main(String[] args) {
+            int count = 1;
+    
+            while (count <= 5) {
+                System.out.println("Count: " + count);
+                count++;
+            }
+        }
+    }
+
