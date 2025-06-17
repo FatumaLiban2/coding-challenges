8523169a21dd872610fd1ed4d94b14707de54b9d
FatumaLiban2 <fatuma.omar@strathmore.edu>
2025-06-17 12:48:11 +0300

    Add files via upload

---
+++ ArrayChallenges/ArrayChallenge1.java
@@ -0,0 +1,6 @@
+public class ArrayChallenge{
+    public static void main(String[] args){
+        String orderIds={ "A3232", "B4743", "C5345","B4398","A9839","C1923"};
+        for (String item)
+    }
+}
