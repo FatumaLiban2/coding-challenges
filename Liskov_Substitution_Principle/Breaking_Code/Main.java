dd7670f6a77db35a61058775636f7d681be31519
FatumaLiban2 <fatuma.omar@strathmore.edu>
2025-07-07 00:21:39 +0300

    Update Main.java

--- Liskov_Substitution_Principle/Breaking_Code/Main.java
+++ Liskov_Substitution_Principle/Breaking_Code/Main.java
@@ -1,6 +1,6 @@
 package Liskov_Substitution_Principle.Breaking_Code;
 
-public class Main {
+public class Main{
         public static void main(String[] args) {
         Bird myBird = new Ostrich(); // Still treated as a Bird
         myBird.fly(); // Throws exception! "Ostrich can't fly"
